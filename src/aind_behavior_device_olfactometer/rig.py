ce3dbddd77b1c8df50ee3584e7a8b26608686fbb
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-12-11 04:42:55 -0800

    Linting

--- src/aind_behavior_device_olfactometer/rig.py
+++ src/aind_behavior_device_olfactometer/rig.py
@@ -1,10 +1,11 @@
-from typing import Literal, Optional
+from typing import Literal
 
 from aind_behavior_services.calibration import olfactometer as olf
 from aind_behavior_services.rig import AindBehaviorRigModel
 from pydantic import Field
 
 __version__ = "0.1.0"
+
 
 class OlfactometerCalibrationRig(AindBehaviorRigModel):
     version: Literal[__version__] = __version__
