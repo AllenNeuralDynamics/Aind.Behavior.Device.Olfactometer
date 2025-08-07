9571ebe0bf0fc462c8a5b2ecdecd461a553b4e3d
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-08-07 08:40:37 -0700

    Update ecosystem dependencies

--- src/aind_behavior_device_olfactometer/rig.py
+++ src/aind_behavior_device_olfactometer/rig.py
@@ -4,17 +4,11 @@ from aind_behavior_services import rig
 from aind_behavior_services.calibration import olfactometer as olf
 from pydantic import Field
 
-__version__ = "0.2.0"
-
-
-class HarpOlfactometer(rig.HarpOlfactometer):
-    """Overrides the default settings for the olfactometer calibration"""
-
-    calibration: olf.OlfactometerCalibration = Field(description="Olfactometer calibration")
+from . import __version__
 
 
 class OlfactometerCalibrationRig(rig.AindBehaviorRigModel):
     version: Literal[__version__] = __version__
-    harp_olfactometer: HarpOlfactometer = Field(..., title="Olfactometer device")
-    harp_analog_input: rig.HarpAnalogInput = Field(..., title="Analog input device")
-    harp_clock_generator: rig.HarpWhiteRabbit = Field(..., title="Clock generator device")
+    harp_olfactometer: olf.Olfactometer = Field(..., title="Olfactometer device")
+    harp_analog_input: rig.harp.HarpAnalogInput = Field(..., title="Analog input device")
+    harp_clock_generator: rig.harp.HarpWhiteRabbit = Field(..., title="Clock generator device")
